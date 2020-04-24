0fd1e6e239c9f3aa4b6651a415f8d8b05522687e
hermtechs <38890522+hermtechs@users.noreply.github.com>
2020-04-24 21:23:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const readMorebtn = document.getElementsByClassName("read-more-btn")
+"use strict";
+var readMoreBtns =document.getElementsByClassName("ReadBtn");
+for(var i = 0; i<readMoreBtns.length; i++){
+    var eachBtn = readMoreBtns[i];
+    //console.log(eachBtn);
+    eachBtn.addEventListener("click", ReadMore)
+}
+
+function ReadMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
