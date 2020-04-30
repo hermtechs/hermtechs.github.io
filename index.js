b6f7500047337026aa0a6f1d18ab49b7aae15c1c
hermtechs <38890522+hermtechs@users.noreply.github.com>
2020-04-30 18:49:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ function hideSideBar(){
 const entireSideBar = document.getElementsByClassName('sidebar');
 entireSideBar[0].classList.add("hideit");
 }
-/*
-var readMoreBtns =document.getElementsByClassName("ReadBtn");
+//
+/*var readMoreBtns =document.getElementsByClassName("ReadBtn");
 for(var i = 0; i<readMoreBtns.length; i++){
     var eachBtn = readMoreBtns[i];
     //console.log(eachBtn);
-    eachBtn.addEventListener("click", ReadMore)
+    eachBtn.addEventListener("click", function ReadMore(event){
+     var clickedbtn = event.target;
+     clickedbtn.addEventListener("click", ReadMore);
+    } )
 }
+  /*var btn1 =document.getElementsByClassName("readBtn1")
+    var btn2 =document.getElementsByClassName("readBtn2")
+    var btn3 =document.getElementsByClassName("readBtn3")
+    var btn4 =document.getElementsByClassName("readBtn4")
+    var btn5 =document.getElementsByClassName("readBtn5")
+    var btn6 =document.getElementsByClassName("readBtn6")
+  */
 
-function ReadMore() {
+/*function ReadMore(event) {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
@@ -30,5 +40,4 @@ function ReadMore() {
       btnText.innerHTML = "Read less";
       moreText.style.display = "inline";
     }
-  }
-*/
+  }*/
