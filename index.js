f213d07c312376b483d4016ea8c19267646fbb8c
hermtechs <38890522+hermtechs@users.noreply.github.com>
2020-05-02 21:00:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
 "use strict";
-function showSideBar(){
-  const entireSideBar = document.getElementsByClassName('sidebar');
-  entireSideBar[0].classList.remove("default-hide");
-}
-
-function hideSideBar(){
-const entireSideBar = document.getElementsByClassName('sidebar');
-entireSideBar[0].classList.add("hideit");
-}
-//
-var readMoreBtns =document.getElementsByClassName("ReadBtn");
-for(var i = 0; i<readMoreBtns.length; i++){
-    var eachBtn = readMoreBtns[i];
-    //console.log(eachBtn);
-    eachBtn.addEventListener("click", function ReadMore(event){
-     var clickedbtn = event.target;
-     clickedbtn.addEventListener("click", ReadMore);
-    } )
-}
- 
-function ReadMore(event) {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
-  }
+//AutoUpdating the Year of Copyright
+const x = new Date();
+const currentYear = x.getFullYear();
+const copyrightYear = document.getElementById("the-year");
+copyrightYear.innerHTML = currentYear;
