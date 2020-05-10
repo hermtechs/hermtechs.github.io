d5ae69a381e97b374adbb65f7d394243543ce6b3
hermtechs <38890522+hermtechs@users.noreply.github.com>
2020-05-10 22:15:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const copyrightYear = document.getElementById("the-year");
 copyrightYear.innerHTML = currentYear;
 
 //The ReadMore-ReadLess Button
-var hiddenText = document.getElementsByClassName("more");
+/*var hiddenText = document.getElementsByClassName("more");
 for(var i=0; i<hiddenText.length; i++)
 {
 var eachHiddenTexts = hiddenText[i];
@@ -43,4 +43,27 @@ function changeDisplay()
       eachHiddenTexts.style.display ="inline";
       eachBtn.innerHTML = "Read Less";
     }
+}*/
+var ReadButton = document.querySelectorAll(".ReadBtn");
+var hiddenText = document.querySelectorAll(".hide");
+var dots = document.querySelectorAll(".dots");
+//console.log(hiddenText)
+//console.log(ReadButton);
+for(var i = 0; i<ReadButton.length; i++){
+ReadButton[i].addEventListener("click", ShowText);
+//ReadButton[i].style.display = "none";
+}
+function ShowText()
+{
+  for(var i = 0; i<hiddenText.length; i++){
+    //console.log(hiddenText[0])
+    hiddenText[i].classList.remove("hide");
+  }
+  for(var i = 0; i<ReadButton.length; i++){
+   // console.log(ReadButton[i]);
+    ReadButton[i].style.display = "none";
+}
+  for(var i=0; i<dots.length; i++){
+    dots[i].style.display = "none";
+  }
 }
