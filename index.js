f494d31669bba0e03b5cbe764fb29094c9d52ad2
hermtechs <38890522+hermtechs@users.noreply.github.com>
2020-05-31 23:45:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,44 +6,7 @@ const copyrightYear = document.getElementById("the-year");
 copyrightYear.innerHTML = currentYear;
 
 //The ReadMore-ReadLess Button
-/*var hiddenText = document.getElementsByClassName("more");
-for(var i=0; i<hiddenText.length; i++)
-{
-var eachHiddenTexts = hiddenText[i];
-//console.log(eachHiddenTexts);
-}
 
-var ReadButton = document.getElementsByClassName("ReadBtn");
-//console.log(ReadButton)
-for(var i=0; i<ReadButton.length; i++)
-{
-  var eachBtn = ReadButton[i];
-  eachBtn.addEventListener("click", changeDisplay)
-}
-var dots = document.getElementsByClassName("dots");
-for (var i=0; i<dots.length; i++)
-{
-  var eachDots = dots[i]
-  //console.log(eachDots);
-}
-function changeDisplay()
-{
-  console.log(eachDots);
-   //eachDots.style.display = "none";
-   // console.log(eachDots.style.display);
-    if(eachDots.style.display === 'none')
-    {
-      eachDots.style.display ="inline";
-      eachHiddenTexts.style.display="none";
-      eachBtn.innerHTML = "Read more";
-    }
-    else
-    {
-      eachDots.style.display = "none";
-      eachHiddenTexts.style.display ="inline";
-      eachBtn.innerHTML = "Read Less";
-    }
-}*/
 var ReadButton = document.querySelectorAll(".ReadBtn");
 var hiddenText = document.querySelectorAll(".hide");
 var dots = document.querySelectorAll(".dots");
@@ -66,4 +29,42 @@ function ShowText()
   for(var i=0; i<dots.length; i++){
     dots[i].style.display = "none";
   }
+}
+
+
+//SHARE BUTTON
+//Getting all share buttons
+const shareBtn = document.querySelectorAll(".share");
+//console.log(shareBtn);
+for(var i=0; i<shareBtn.length; i++){
+  shareBtn[i].addEventListener("click", shareApi);
+}
+//Auto-generating url for each news post ie div
+function shareApi(event){
+  var TheClickedBtn = event.target.parentElement;
+  var gettingHref = TheClickedBtn.getAttribute("href");
+  gettingHref.toString;
+ //console.log(gettingHref)
+  var TheUrl = "https://weareuganda.com"+gettingHref; 
+ // console.log(TheUrl)
+  
+//calling webshare API
+if(navigator.share){
+  navigator.share(
+      
+    {
+      title: 'webshare Api exmple',
+      url: TheUrl ,
+      text: 'We are Uganda News'
+    }
+  ).then(
+    () =>{
+      console.log("Thanks for sharing");
+    }
+  )
+  .catch(console.error);
+}
+else{
+  console.log("not supported");
+}
 }
